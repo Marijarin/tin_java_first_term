d4470bec3c6ee2583a5ce92b8f0eb21e783de1df
Marina Zharinova <phyrebirrd@gmail.com>
2023-10-28 00:10:51 +0500

    do up to 19 task

--- src/main/java/edu/hw4/AnimalOperations.java
+++ src/main/java/edu/hw4/AnimalOperations.java
@@ -17,9 +17,19 @@ public class AnimalOperations {
         new Animal("Zuza", Animal.Type.DOG, Animal.Sex.F, 1, 30, 3, false),
         new Animal("Popka Durak Pervyi", Animal.Type.BIRD, Animal.Sex.M, 1, 10, 2, false),
         new Animal("Guppy", Animal.Type.FISH, Animal.Sex.F, 2, 5, 1, false),
-        new Animal("Spider", Animal.Type.SPIDER, Animal.Sex.M, 11, 1, 1, true),
-        new Animal("DangerousSpider", Animal.Type.SPIDER, Animal.Sex.F, 0, 35, 3, true),
+        new Animal("Spider", Animal.Type.SPIDER, Animal.Sex.M, 8, 1, 1, true),
+        new Animal("Dangerous Spider", Animal.Type.SPIDER, Animal.Sex.F, 0, 35, 3, true),
         new Animal("Shark", Animal.Type.FISH, Animal.Sex.F, 20, 1000, 3000, true),
+        new Animal("Sharik", Animal.Type.DOG, Animal.Sex.M, 8, 100, 35, true)
+    );
+    public static final List<Animal> ANIMALS_EXAMPLE_SECOND = List.of(
+        new Animal("Murka", Animal.Type.CAT, Animal.Sex.M, 4, 40, 6, false),
+        new Animal("Zazz", Animal.Type.FISH, Animal.Sex.F, 1, 300, 350, false),
+        new Animal("Popka Durak Pervyi", Animal.Type.BIRD, Animal.Sex.M, 1, 10, 2, false),
+        new Animal("Guppy", Animal.Type.FISH, Animal.Sex.F, 2, 5, 1, false),
+        new Animal("Spider", Animal.Type.SPIDER, Animal.Sex.M, 11, 1, 1, true),
+        new Animal("Dangerous Spider", Animal.Type.SPIDER, Animal.Sex.F, 0, 35, 3, true),
+        new Animal("Shark", Animal.Type.FISH, Animal.Sex.F, 20, 1000, 1000, true),
         new Animal("Sharik", Animal.Type.DOG, Animal.Sex.M, 8, 100, 35, true)
     );
 
@@ -40,7 +50,8 @@ public class AnimalOperations {
             .entrySet()
             .stream()
             .map(k ->
-                entry(k.getKey(), k.getValue().size())).collect(Collectors.toMap(Map.Entry::getKey,
+                entry(k.getKey(), k.getValue().size())).collect(Collectors.toMap(
+                Map.Entry::getKey,
                 Map.Entry::getValue
             ));
     }
@@ -64,11 +75,13 @@ public class AnimalOperations {
 
     //#6
     public static Map<Animal.Type, Integer> heaviestAnimal(List<Animal> animals) {
-        return animals.stream().sorted(Comparator.comparing(Animal::weight).reversed()).collect(Collectors.groupingBy(Animal::type))
+        return animals.stream().sorted(Comparator.comparing(Animal::weight).reversed())
+            .collect(Collectors.groupingBy(Animal::type))
             .entrySet()
             .stream()
             .map(k ->
-                entry(k.getKey(), k.getValue().getFirst().weight())).collect(Collectors.toMap(Map.Entry::getKey,
+                entry(k.getKey(), k.getValue().getFirst().weight())).collect(Collectors.toMap(
+                Map.Entry::getKey,
                 Map.Entry::getValue
             ));
     }
@@ -90,34 +103,72 @@ public class AnimalOperations {
 
     //#10
     public static List<Animal> ageNumberNotPawsNumber(List<Animal> animals) {
-        return animals.stream().filter(animal -> animal.age()!= animal.paws()).toList();
+        return animals.stream().filter(animal -> animal.age() != animal.paws()).toList();
     }
 
     //#11
     public static List<Animal> canByteAndMoreThan100(List<Animal> animals) {
-        return animals.stream().filter(animal -> animal.bites() && animal.height()>100).toList();
+        return animals.stream().filter(animal -> animal.bites() && animal.height() > 100).toList();
     }
 
     //#12
-    public static int numberAnimalsWithWeightMoreHeight(List<Animal> animals) {
-        return animals.stream().filter(animal -> animal.weight()>animal.height()).toList().size();
+    public static int numberAnimalsWeightMoreHeight(List<Animal> animals) {
+        return animals.stream().filter(animal -> animal.weight() > animal.height()).toList().size();
     }
 
     //#13
-    public static List<Animal> namesInMoreTwoWords(List<Animal> animals){
+    public static List<Animal> namesInMoreTwoWords(List<Animal> animals) {
         return animals.stream().filter(animal -> animal.name().split(" ").length > 2).toList();
     }
 
     //#14
-    public static boolean IsDogMoreKHeight(List<Animal> animals, int k){
-        return animals.stream().filter(animal -> animal.type() == Animal.Type.DOG && animal.height() > k).toList().isEmpty();
+    public static boolean isDogMoreKHeight(List<Animal> animals, int k) {
+        return animals.stream().filter(animal -> animal.type() == Animal.Type.DOG && animal.height() > k).toList()
+            .isEmpty();
     }
 
     //#15
     public static int sumWeightFromKToLAge(List<Animal> animals, int k, int l) {
-        return animals.stream().filter(animal -> animal.age() < l && animal.age() > k).map(Animal::weight).reduce(0, Integer::sum);
+        return animals.stream().filter(animal -> animal.age() < l && animal.age() > k).map(Animal::weight)
+            .reduce(0, Integer::sum);
     }
 
     //#16
-    
+    public static List<Animal> tripleSorting(List<Animal> animals) {
+        return animals.stream()
+            .sorted(Comparator.comparing(Animal::type)
+                .thenComparing(Animal::sex)
+                .thenComparing(Animal::name))
+            .toList();
+    }
+
+    //#17
+    public static boolean spidersBitesMoreDogs(List<Animal> animals) {
+        return animals.stream()
+            .filter(animal ->
+                animal.type() == Animal.Type.DOG && animal.bites() ||
+                    animal.type() == Animal.Type.SPIDER && animal.bites())
+            .collect(Collectors.groupingBy(Animal::type))
+            .entrySet()
+            .stream()
+            .max(Comparator.comparing(typeListEntry -> typeListEntry.getValue().size()))
+            .map(Map.Entry::getKey)
+            .orElse(Animal.Type.DOG) == Animal.Type.SPIDER;
+    }
+
+    //#18
+    public static Animal heaviestFishEver(List<List<Animal>> animalLists) {
+        return animalLists.stream()
+            .filter(animalList ->
+                !animalList.stream()
+                    .filter(animal -> animal.type() == Animal.Type.FISH)
+                    .toList()
+                    .isEmpty())
+            .flatMap(Collection::stream)
+            .max(Comparator.comparing(Animal::weight))
+            .orElseThrow();
+    }
+
+    //#19
+
 }
--- src/main/java/edu/hw4/Main.java
+++ src/main/java/edu/hw4/Main.java
@@ -4,11 +4,25 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import java.util.List;
 import static edu.hw4.AnimalOperations.ANIMALS_EXAMPLE;
+import static edu.hw4.AnimalOperations.ANIMALS_EXAMPLE_SECOND;
+import static edu.hw4.AnimalOperations.ageNumberNotPawsNumber;
+import static edu.hw4.AnimalOperations.allPawsNumber;
+import static edu.hw4.AnimalOperations.canByteAndMoreThan100;
 import static edu.hw4.AnimalOperations.fromHeavyToLight;
 import static edu.hw4.AnimalOperations.fromSmallToBig;
 import static edu.hw4.AnimalOperations.heaviestAnimal;
+import static edu.hw4.AnimalOperations.heaviestFishEver;
+import static edu.hw4.AnimalOperations.isDogMoreKHeight;
 import static edu.hw4.AnimalOperations.longestName;
+import static edu.hw4.AnimalOperations.mostFrequent;
+import static edu.hw4.AnimalOperations.mostHeaviestKAnimalLowerK;
+import static edu.hw4.AnimalOperations.mostOldKAnimal;
+import static edu.hw4.AnimalOperations.namesInMoreTwoWords;
+import static edu.hw4.AnimalOperations.numberAnimalsWeightMoreHeight;
 import static edu.hw4.AnimalOperations.speciesRange;
+import static edu.hw4.AnimalOperations.spidersBitesMoreDogs;
+import static edu.hw4.AnimalOperations.sumWeightFromKToLAge;
+import static edu.hw4.AnimalOperations.tripleSorting;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -27,5 +41,18 @@ public final class Main {
         LOGGER.info(speciesRange(ANIMALS_EXAMPLE));
         LOGGER.info(longestName(ANIMALS_EXAMPLE));
         LOGGER.info(heaviestAnimal(ANIMALS_EXAMPLE));
+        LOGGER.info(mostOldKAnimal(ANIMALS_EXAMPLE, 3));
+        LOGGER.info(mostHeaviestKAnimalLowerK(ANIMALS_EXAMPLE, 20));
+        LOGGER.info(allPawsNumber(ANIMALS_EXAMPLE));
+        LOGGER.info(ageNumberNotPawsNumber(ANIMALS_EXAMPLE));
+        LOGGER.info(mostFrequent(ANIMALS_EXAMPLE));
+        LOGGER.info(canByteAndMoreThan100(ANIMALS_EXAMPLE));
+        LOGGER.info(numberAnimalsWeightMoreHeight(ANIMALS_EXAMPLE));
+        LOGGER.info(namesInMoreTwoWords(ANIMALS_EXAMPLE));
+        LOGGER.info(isDogMoreKHeight(ANIMALS_EXAMPLE, 10));
+        LOGGER.info(sumWeightFromKToLAge(ANIMALS_EXAMPLE, 0, 2));
+        LOGGER.info(tripleSorting(ANIMALS_EXAMPLE));
+        LOGGER.info(spidersBitesMoreDogs(ANIMALS_EXAMPLE));
+        LOGGER.info(heaviestFishEver(List.of(ANIMALS_EXAMPLE, ANIMALS_EXAMPLE_SECOND)));
     }
 }
