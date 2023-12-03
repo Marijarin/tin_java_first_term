66f8004bfebcf221b3c986eff300dba6cd9d1eaf
Marina Zharinova <phyrebirrd@gmail.com>
2023-12-03 22:29:24 +0500

    tests corrected

--- src/test/java/edu/hw_8/Task1FirstTest.java
+++ src/test/java/edu/hw_8/Task1FirstTest.java
@@ -3,15 +3,19 @@ package edu.hw_8;
 import edu.hw_8.Task1.Client;
 import edu.hw_8.Task1.RoutingMessageHandler;
 import edu.hw_8.Task1.Server;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(OutputCaptureExtension.class)
 public class Task1FirstTest {
+
     @Test
     void answerFromServerIsCorrectWhenCorrectInput(CapturedOutput output) throws InterruptedException, IOException {
         Client client1 = new Client();
@@ -33,15 +37,11 @@ public class Task1FirstTest {
         client1.send(test.getBytes());
         client1.waitResponse();
         Thread.sleep(1000);
-        if (!output.isEmpty()) {
-            assertThat(output).contains("Ты просто бог идиотизма.");
-        } else {
-            Thread.sleep(1000);
-            assertThat(output).contains("Ты просто бог идиотизма.");
-        }
-
+        assertThat(output).contains("Ты просто бог идиотизма.");
         client1.close();
         server.isRunning = false;
         serverTread.interrupt();
+        System.out.close();
     }
+
 }
--- src/test/java/edu/hw_8/Task1SecondTest.java
+++ src/test/java/edu/hw_8/Task1SecondTest.java
@@ -3,17 +3,46 @@ package edu.hw_8;
 import edu.hw_8.Task1.Client;
 import edu.hw_8.Task1.RoutingMessageHandler;
 import edu.hw_8.Task1.Server;
+import java.io.IOException;
+import java.util.logging.LogManager;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
-import java.io.IOException;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(OutputCaptureExtension.class)
 public class Task1SecondTest {
-    @Test void answerFromServerIsCorrectWhenNotCorrectInput(CapturedOutput output1)
+
+    @Test void answerFromServerIsCorrectWhenNotCorrectInput(CapturedOutput output)
         throws InterruptedException, IOException {
+        Client client1 = new Client();
+        RoutingMessageHandler routingMessageHandler = new RoutingMessageHandler(1024);
+        Server server = new Server(routingMessageHandler);
+        Thread serverTread = new Thread(() -> {
+            try {
+                server.start();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        serverTread.start();
+        Thread.sleep(1000);
+        client1.start();
+
+        String test = "глупыйй";
+
+        client1.send(test.getBytes());
+        client1.waitResponse();
+        assertThat(output).contains("Не могу прислать подходящую фразу, попробуй другое слово");
+        client1.close();
+        server.isRunning = false;
+        serverTread.interrupt();
+    }
+
+    @Test
+    void answerFromServerIsCorrectWhenCorrectInput(CapturedOutput output) throws InterruptedException, IOException {
         Client client1 = new Client();
         RoutingMessageHandler routingMessageHandler = new RoutingMessageHandler(1024);
         Server server = new Server(routingMessageHandler);
@@ -28,21 +57,19 @@ public class Task1SecondTest {
         Thread.sleep(100);
         client1.start();
 
-        String test = "глупыйй";
+        String test = "глупый";
 
         client1.send(test.getBytes());
         client1.waitResponse();
         Thread.sleep(1000);
-
-        if (!output1.isEmpty()) {
-            assertThat(output1).containsOnlyOnce(test);
-        } else {
-            Thread.sleep(1000);
-            assertThat(output1).containsOnlyOnce(test);
-        }
-
+        assertThat(output).contains("Ты просто бог идиотизма.");
         client1.close();
         server.isRunning = false;
         serverTread.interrupt();
+    }
+
+    @AfterEach
+    void reset() throws IOException {
+        LogManager.getLogManager().readConfiguration();
     }
 }
