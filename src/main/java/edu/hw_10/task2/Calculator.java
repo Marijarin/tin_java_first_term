cc524f26af45c699a986288ea0dae301f502cfff
Marina Zharinova <phyrebirrd@gmail.com>
2023-12-12 11:58:38 +0500

    task2 start

---
+++ src/main/java/edu/hw_10/task2/Calculator.java
@@ -0,0 +1,6 @@
+package edu.hw_10.task2;
+
+public interface Calculator {
+   // @Cache(persist = true)
+    public long fib(int number);
+}
---
+++ src/main/java/edu/hw_10/task2/FibCalculator.java
@@ -0,0 +1,8 @@
+package edu.hw_10.task2;
+
+public class FibCalculator implements Calculator{
+    @Override
+    public long fib(int number) {
+        return 0;
+    }
+}
